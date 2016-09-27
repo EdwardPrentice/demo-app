4ba6afde1a8f27627837e368326c01cc456f8731
Edward Prentice <edwardprentice@gmail.com>
2016-09-27 21:13:30 +0100

    Hacking away on the web.js added hello world route

--- web.js
+++ web.js
@@ -4,10 +4,14 @@ var morgan = require('morgan');
 var app = express();
 
 app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist/public"));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
 app.use(function(req, res){
     res.sendFile(__dirname + "/dist/public/index.html");
+});
+
+app.get('/', function(request, response) {
+      response.send('Hello World!');
 });
 
 app.listen(process.env.PORT || 5000);
